f32111aafb0da3893ee50ebfa4a99520b50ab234
Elm3adawy <amrelmaadawy905@gmail.com>
2025-09-22 20:10:40 +0300

    add map

--- lib/features/air_quality_map/UI/air_quality_map_view.dart
+++ lib/features/air_quality_map/UI/air_quality_map_view.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class AirQualityMapView extends StatelessWidget {
-  const AirQualityMapView({super.key});
+  AirQualityMapView({super.key});
+
+  // بيانات تلوث (Hardcoded)
+  final List<Map<String, dynamic>> pollutionData = [
+    {
+      "location": LatLng(30.0444, 31.2357), // القاهرة
+      "level": "High",
+    },
+    {
+      "location": LatLng(29.9668, 32.5498), // السويس
+      "level": "Medium",
+    },
+    {
+      "location": LatLng(31.2001, 29.9187), // الإسكندرية
+      "level": "Low",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return 
+    
+  FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(30.0444, 31.2357), // مركز القاهرة
+          initialZoom: 6.5,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            userAgentPackageName: "com.example.pollution_map",
+          ),
+          CircleLayer(
+            circles: pollutionData.map((point) {
+              double radius;
+              Color color;
+
+              switch (point["level"]) {
+                case "High":
+                  radius = 50000000; // نصف قطر (متر)
+                  color = Colors.red.withValues(alpha:0.4);
+                  break;
+                case "Medium":
+                  radius = 30000000;
+                  color = Colors.orange.withValues(alpha:0.4);
+                  break;
+                default:
+                  radius = 20000000;
+                  color = Colors.green.withValues(alpha:0.4);
+              }
+
+              return CircleMarker(
+                point: point["location"],
+                radius: radius / 1000, // flutter_map بيقيس بالكيلومتر تقريبًا
+                useRadiusInMeter: true,
+                color: color,
+                borderStrokeWidth: 2,
+                borderColor: Colors.red.shade900,
+              );
+            }).toList(),
+          ),
+        ],
+      );
+    
   }
 }
