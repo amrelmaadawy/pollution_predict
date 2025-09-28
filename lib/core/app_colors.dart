2ac6225f470f5188fc55ec675d7bdf6cbb4f8fd4
Elm3adawy <amrelmaadawy905@gmail.com>
2025-09-28 22:14:12 +0300

    fix highlight colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 const kLightPrimaryColor = Color(0XFF4F964F);
 const kPrimaryBlackColor = Color(0XFF0D1C0D);
-const kYellowTextColor = Color(0XFFFF9800);
-const kRedTextColor = Color(0XFFF44336);
-const kLightHighlightYellowColor = Color(0X33FFD600);
-const kLightHighlightRedColor = Color(0X33F44336);
+final kYellowTextColor = Colors.yellow.shade700;
+final kRedTextColor = Colors.red.shade700;
+final kLightHighlightYellowColor = Colors.yellow.shade100;
+final kLightHighlightRedColor = Colors.red.shade100;
 const kLightBorderColor = Color(0xFFB0BEC5);
 const kSubTextColor = Color(0xFF455A64);
 final kLightHighlightGreenColor = Colors.green.shade100;
@@ -15,10 +15,14 @@ final klightGreenTextColor = Colors.green.shade700;
 // 🎨 Dark Mode Colors
 const kDarkPrimaryColor = Color(0xFF2E7D32); // أخضر غامق كـ primary
 const kPrimaryWhiteColor = Color(0xFFECEFF1); // أبيض فاتح للنصوص
-const kDarkYellowTextColor = Color(0xFFFFB74D); // أصفر هادي
-const kDarkRedTextColor = Color(0xFFE57373); // أحمر غامق هادي
-const kDarkHighlightYellowColor = Color(0x66FFD600); // أصفر شفاف للـ highlight
-const kDarkHighlightRedColor = Color(0x66F44336); // أحمر شفاف للـ highlight
+const kDarkYellowTextColor = Colors.yellowAccent; // أصفر هادي
+const kDarkRedTextColor = Colors.redAccent; // أحمر غامق هادي
+final kDarkHighlightYellowColor = Colors.yellowAccent.withValues(
+  alpha: 0.2,
+); // أصفر شفاف للـ highlight
+final kDarkHighlightRedColor = Colors.redAccent.withValues(
+  alpha: 0.2,
+); // أحمر شفاف للـ highlight
 const kDarkBorderColor = Color(0xFF37474F); // رمادي غامق كـ border
 const kDarkSubTextColor = Color(0xFF90A4AE); // رمادي فاتح للنصوص الثانوية
 final kDarkHighlightGreenColor = Colors.greenAccent.withValues(
